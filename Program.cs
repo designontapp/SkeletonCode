d6accd5ac73644c72138ed3343c915541f7dcadb
Daniel Tapp <175604229+designontapp@users.noreply.github.com>
2025-01-23 12:57:57 +0000

    Complete

--- Program.cs
+++ Program.cs
@@ -119,14 +119,14 @@ namespace TargetClearCS
 
         static void UpdateTargets(List<int> Targets, bool TrainingGame, int MaxTarget)
         {
-            for (int Count = 0; Count < Targets.Count - 1; Count++)
-            {
-                Targets[Count] = Targets[Count + 1];
-            }
-            Targets.RemoveAt(Targets.Count - 1);
+            //for (int Count = 0; Count < Targets.Count - 1; Count++)
+            //{
+            //    Targets[Count] = Targets[Count + 1];
+            //}
+            Targets.RemoveAt(Targets.Count() - 1);
             if (TrainingGame)
             {
-                Targets.Add(Targets[Targets.Count - 1]);
+                Targets.Add(Targets[Targets.Count() - 1]);
             }
             else
             {
